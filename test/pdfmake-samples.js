784cdb1b1c891d61ec6749d10fb170831350f857
= <=>
2023-01-18 02:07:35 -0500

    added basics sample

---
+++ test/pdfmake-samples.js
@@ -0,0 +1,30 @@
+//this is testing the samples provided in pdfmake webiste
+//http://pdfmake.org/playground.html
+
+const expect = require('chai').expect;
+const { getDD , render} = require('../index');
+const path = require('path');
+
+describe('basics example', () => {
+    const DD = {
+        content: [
+            { text : 'First paragraph'},
+            { text : 'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'}
+        ]
+        
+    };
+
+    it('Generated properly', async () => {
+        let pdfDoc = await render({
+            path: path.join(__dirname, '../samples/pdfmake-samples/basics.pdfml'),
+            data: {  }
+        });
+        expect(pdfDoc).to.be.ok;
+    });
+
+    it('PDF Content matches', async () => {
+        let _DD = await getDD(path.join(__dirname, '../samples/pdfmake-samples/basics.pdfml'), {});
+        expect(_DD.content).to.deep.equal(DD.content);
+    });
+
+});
